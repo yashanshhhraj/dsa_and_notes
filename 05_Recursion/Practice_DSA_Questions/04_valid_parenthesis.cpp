571c1f2828b960b5295848b158ce600a234c70bf
Yashansh Raj Pandey <pandeyyashanshraj000@gmail.com>
2025-07-09 08:35:42 +0530

    Add Generate Parenthesis

--- 05_Recursion/Practice_DSA_Questions/04_valid_parenthesis.cpp
+++ 05_Recursion/Practice_DSA_Questions/04_valid_parenthesis.cpp
@@ -2,27 +2,33 @@
 #include<vector>
 using namespace std;
 
-void validParenthesis(int n, int l, int r, vector<vector<string>>& ans, vector<string> & temp) {
-    if(l == n || r == n || r > l) {
+void validParenthesis(int n, int l, int r, vector<string>& ans, string & temp) {
+    if(l == n && r == n) {
+        ans.push_back(temp);
         return;
     }
 
     // ? Adding (
-    temp.push_back('(');
-    validParenthesis(n, l + 1, r, ans, temp);
-    
-    // ? Adding )
-    temp.push_back(')');
-    validParenthesis(n, l, r + 1, ans, temp);
+    if(l < n) {
+        temp.push_back('(');
+        validParenthesis(n, l + 1, r, ans, temp);
+        temp.pop_back();
+    }
+
+    if(r < l){
+        // ? Adding )
+        temp.push_back(')');
+        validParenthesis(n, l, r + 1, ans, temp);
+        temp.pop_back();
+    }
 }
 
 int main() {
-    int n = 3;
-    vector<vector<string>> ans;
-    vector<string> temp;
+    int n = 2;
+    vector<string> ans;
+    string temp;
     validParenthesis(n, 0, 0, ans, temp);
 
     for(int i = 0; i < ans.size(); i++)
-    for(int j = 0; j < ans[i].size(); j++)
-    cout << ans[i][j] << ", ";
+    cout << ans[i] << ", ";
 }
