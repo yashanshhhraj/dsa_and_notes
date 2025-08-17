f9f976d6382e0257062a4e6602f3b55f2f42cccd
Yashansh Raj Pandey <pandeyyashanshraj000@gmail.com>
2025-08-17 16:06:08 +0530

    Merge K sorted LinkedList

--- 07_linked_list/03_que_solution.cpp
+++ 07_linked_list/03_que_solution.cpp
@@ -1,99 +1,99 @@
-// ! Merge k sorted lists solution
-// ? Time Complexity : O(NM)
-class Solution {
-public:
-    ListNode * mergeLinkedList(ListNode * head1, ListNode * head2) {
-        ListNode * head = new ListNode(-1);
-        ListNode * tail = head;
+// // ! Merge k sorted lists solution
+// // ? Time Complexity : O(NM)
+// class Solution {
+// public:
+//     ListNode * mergeLinkedList(ListNode * head1, ListNode * head2) {
+//         ListNode * head = new ListNode(-1);
+//         ListNode * tail = head;
 
-        while(head1 && head2) {
-            if(head1 -> val <= head2 -> val) {
-                tail -> next = head1;
-                tail = tail -> next;
-                head1 = head1 -> next;
-            }
+//         while(head1 && head2) {
+//             if(head1 -> val <= head2 -> val) {
+//                 tail -> next = head1;
+//                 tail = tail -> next;
+//                 head1 = head1 -> next;
+//             }
 
-            else {
-                tail -> next = head2;
-                tail = tail -> next;
-                head2 = head2 -> next;
-            }
-        }
+//             else {
+//                 tail -> next = head2;
+//                 tail = tail -> next;
+//                 head2 = head2 -> next;
+//             }
+//         }
 
-        if(head1) tail -> next = head1;
-        if(head2) tail -> next = head2;
+//         if(head1) tail -> next = head1;
+//         if(head2) tail -> next = head2;
 
-        ListNode * temp = head;
-        head = head -> next;
-        delete temp;
+//         ListNode * temp = head;
+//         head = head -> next;
+//         delete temp;
 
-        return head;
-    }
+//         return head;
+//     }
 
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(!lists.size()) return nullptr;
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         if(!lists.size()) return nullptr;
 
-        ListNode * head = lists[0];
+//         ListNode * head = lists[0];
 
-        for(int i = 1; i < lists.size(); i++)
-        head = mergeLinkedList(head, lists[i]);
+//         for(int i = 1; i < lists.size(); i++)
+//         head = mergeLinkedList(head, lists[i]);
 
-        return head;
-    }
-};
+//         return head;
+//     }
+// };
 
-// ? Using Merge Sort Algo
-class Solution {
-public:
-    ListNode * mergeLinkedList(ListNode * head1, ListNode * head2) {
-        ListNode * head = new ListNode(-1);
-        ListNode * tail = head;
+// // ? Using Merge Sort Algo
+// class Solution {
+// public:
+//     ListNode * mergeLinkedList(ListNode * head1, ListNode * head2) {
+//         ListNode * head = new ListNode(-1);
+//         ListNode * tail = head;
 
-        while(head1 && head2) {
-            if(head1 -> val <= head2 -> val) {
-                tail -> next = head1;
-                tail = tail -> next;
-                head1 = head1 -> next;
-            }
+//         while(head1 && head2) {
+//             if(head1 -> val <= head2 -> val) {
+//                 tail -> next = head1;
+//                 tail = tail -> next;
+//                 head1 = head1 -> next;
+//             }
 
-            else {
-                tail -> next = head2;
-                tail = tail -> next;
-                head2 = head2 -> next;
-            }
-        }
+//             else {
+//                 tail -> next = head2;
+//                 tail = tail -> next;
+//                 head2 = head2 -> next;
+//             }
+//         }
 
-        if(head1) tail -> next = head1;
-        if(head2) tail -> next = head2;
+//         if(head1) tail -> next = head1;
+//         if(head2) tail -> next = head2;
 
-        ListNode * temp = head;
-        head = head -> next;
-        delete temp;
+//         ListNode * temp = head;
+//         head = head -> next;
+//         delete temp;
 
-        return head;
-    }
+//         return head;
+//     }
 
-    void mergeSort(vector<ListNode*>& arr, int start, int end) {
-        if(start >= end) return; // <-- Base Case
+//     void mergeSort(vector<ListNode*>& arr, int start, int end) {
+//         if(start >= end) return; // <-- Base Case
 
-        // Calculating Mid
-        int mid = start + (end - mid) / 2;
+//         // Calculating Mid
+//         int mid = start + (end - mid) / 2;
 
-        // Left Of Array
-        mergeSort(arr, start, mid);
+//         // Left Of Array
+//         mergeSort(arr, start, mid);
 
-        // Right Of Array
-        mergeSort(arr, mid + 1, end);
+//         // Right Of Array
+//         mergeSort(arr, mid + 1, end);
 
-        // Merging the Linked List
-        arr[start] = mergeLinkedList(arr[start], arr[mid + 1]);
-    }
+//         // Merging the Linked List
+//         arr[start] = mergeLinkedList(arr[start], arr[mid + 1]);
+//     }
 
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(!lists.size()) return nullptr;
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         if(!lists.size()) return nullptr;
 
-        mergeSort(lists, 0, lists.size() - 1);
+//         mergeSort(lists, 0, lists.size() - 1);
 
-        return lists[0];
-    }
-};
+//         return lists[0];
+//     }
+// };
